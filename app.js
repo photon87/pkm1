b779474e1300d103aebcd1a8df3840bcdd950814
photon <andy2887will@gmail.com>
2021-02-28 23:45:56 -0500

    updated styles and added ability to choose set by symbol

--- app.js
+++ app.js
@@ -1,11 +1,38 @@
 console.log('Pokemon Card Display Tool by @photon87');
+console.log('https://github.com/photon87/pkm1');
 
 const cardsAPI = 'https://api.pokemontcg.io/v2/cards?q=';
 const testAPI = 'https://api.pokemontcg.io/v2/cards?q=name:charizard';
 const setsAPI = 'https://api.pokemontcg.io/v2/sets'
 
+const pkmnColors = {
+    'Normal': '#AAB09F',
+    'Colorless': '#AAB09F',
+    'Fire': '#EE8130',
+    'Water': '#6390F0',
+    'Electric': '#F7D02C',
+    'Lightning': '#F7D02C',
+    'Grass': '#7AC74C',
+    'Ice': '#96D9D6',
+    'Fighting': '#C22E28',
+    'Poison': '#A33EA1',
+    'Ground': '#E2BF65',
+    'Flying': '#A98FF3',
+    'Psychic': '#F95587',
+    'Bug': '#A6B91A',
+    'Rock': '#B6A136',
+    'Ghost': '#735797',
+    'Dragon': '#6F35FC',
+    'Dark': '#736C75',
+    'Darkness': '#736C75',
+    'Steel': '#B7B7CE',
+    'Metal': '#B7B7CE',
+    'Fairy': '#D685AD'
+}
+
 const cardsDiv = document.querySelector('.cards');
 const filterDiv = document.querySelector('.filter');
+const setLogoDiv = document.querySelector('.setLogoDiv')
 
 const select = document.createElement("select");
 select.onchange = getCards
@@ -14,6 +41,11 @@ getSets();
 //getCards();
 
 function getSets() {
+
+    const setSymbols = document.createElement('div')
+    setSymbols.className = 'setSymbols'
+
+
     fetch(setsAPI)
         .then(res => res.json())
         .then(sets => {
@@ -23,31 +55,48 @@ function getSets() {
             select.name = "sets";
             select.id = "sets"
 
-            var noneOption = document.createElement("option");
+            const noneOption = document.createElement("option");
             noneOption.value = '';
             noneOption.text = '------';
             select.appendChild(noneOption);
 
             for (const val of values) {
-                var option = document.createElement("option");
+                const option = document.createElement("option");
                 option.value = val.id;
                 option.text = val.name;
                 select.appendChild(option);
+
+                const symbol = document.createElement('img');
+                symbol.src = val.images.symbol
+                symbol.title = val.name;
+                symbol.onclick = () => {
+                    clickSymbol(val.id, val.name)
+                };
+                symbol.className = 'setSymbolFilter';
+                setSymbols.appendChild(symbol)
             }
 
-            var label = document.createElement("label");
+            const label = document.createElement("label");
             label.innerHTML = "Choose a set: "
             label.htmlFor = "sets";
 
             filterDiv.appendChild(label).appendChild(select);
-
+            filterDiv.appendChild(setSymbols)
 
         })
+}
+
+function clickSymbol(id, name) {
+    console.log(id, name)
+    select.value = id;
+    select.text = name;
+    getCards();
 }
 
 
 function getCards() {
     cardsDiv.innerHTML = ''
+    setLogoDiv.innerHTML = ''
     if (select.value != '') {
         fetch(`${cardsAPI}set.id:${select.value}`)
             // fetch(testAPI)
@@ -55,9 +104,20 @@ function getCards() {
             .then(cards => {
                 cards.data.sort((a, b) => new Date(b.set.releaseDate) - new Date(a.set.releaseDate))
                 console.log(cards.data);
+
+
+                const setLogo = document.createElement('img')
+                setLogo.className = 'setLogo'
+                setLogo.src = cards.data[0].set.images.logo
+                setLogoDiv.appendChild(setLogo)
+                const setName = document.createElement('span')
+                setName.textContent = cards.data[0].set.name
+                setLogoDiv.appendChild(setName)
+
                 cards.data.forEach(card => {
                     const div = document.createElement('div')
                     div.className = 'card'
+                    div.style = `background: linear-gradient(#000, ${pkmnColors[card.types[0]]});`
 
                     const name = document.createElement('span')
                     name.className = 'cardName'
@@ -112,7 +172,7 @@ function getCards() {
                     }
 
                     const image = document.createElement('img')
-                    image.src = card.images.large
+                    image.src = card.images.small
                     div.appendChild(image)
 
 
